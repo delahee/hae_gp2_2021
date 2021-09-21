499a090e73697433e795a7a6d5c652f198303bdc
David Elahee <david.elahee@gmail.com>
2021-09-21 16:48:03 +0200

    initial add

---
+++ cours 0/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream>
+
+int main()
+{
+    printf("0\n");
+    int zero = 0;
+    printf("%i\n",zero);
+
+    printf("%f\n",0.5f);
+    printf("%s\n","toto");
+   
+    long long int t = 123456789123456789;// 
+	printf("%lld\n", t);
+
+    uint64_t t1 = 123456789123456789;
+	printf("%lld\n", t1);
+
+
+}
