98a28b35e858fa663be2f4f5ad49703cf39dfc23
David Elahee <bm@headbang.club>
2021-11-02 15:06:14 +0100

    add c3

---
+++ cours 3/ConsoleApplication1/Tool.hpp
@@ -0,0 +1,10 @@
+#include <chrono>
+#include <ctime>
+using namespace std;
+using namespace std::chrono;
+inline double getTimeStamp() //retourne le temps actuel en seconde
+{
+	std::chrono::nanoseconds ns =
+		duration_cast<std::chrono::nanoseconds>(system_clock::now().time_since_epoch());
+	return ns.count() / 1000000000.0;
+}
