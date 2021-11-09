ebf5178754fc4244f7403ec6a2463da45a919d51
David Elahee <bm@headbang.club>
2021-11-09 15:04:29 +0100

    add manual catmull

--- cours 4/ConsoleApplication1/ConsoleApplication1.cpp
+++ cours 4/ConsoleApplication1/ConsoleApplication1.cpp
@@ -14,6 +14,70 @@
 #include <SFML/Window.hpp>
 #include "Curve.hpp"
 
+float catmull(float p0 , float p1 , float p2,float p3 , float t ) {
+	auto q = 2.0f * p1;
+	auto t2 = t * t;
+
+	q += (-p0 + p2) * t;
+	q += (2.0f * p0 - 5.0f * p1 + 4.0f * p2 - p3) * t2;
+	q += (-p0 + 3 * p1 - 3 * p2 + p3) * t2 * t;
+
+	return 0.5f * q;
+}
+
+void drawMountain(sf::RenderWindow& window) {
+
+	sf::VertexArray arr;
+	arr.setPrimitiveType(sf::LineStrip);
+	sf::Color col = sf::Color::Green;
+
+	float baseline = 350;
+
+	sf::Vector2f a(0, baseline+50);
+	sf::Vector2f b(400, baseline-150);
+	sf::Vector2f c(1100, baseline +32);
+	sf::Vector2f d(window.getSize().x, baseline);
+	col.a = 100;
+
+	arr.append(sf::Vertex(a, col));
+	arr.append(sf::Vertex(b, col));
+	arr.append(sf::Vertex(c, col));
+	arr.append(sf::Vertex(d, col));
+	window.draw(arr);
+
+	arr.clear();
+	col = sf::Color::Magenta;
+	for (int i = 0; i < 256; ++i) {
+		float t = 1.0f * i / 256;
+		float x = catmull(a.x, a.x, b.x, c.x, t);
+		float y = catmull(a.y, a.y, b.y, c.y, t);
+		arr.append(sf::Vertex(sf::Vector2f(x, y), col));
+	}
+	for (int i = 0; i < 256; ++i) {
+		float t = 1.0f * i / 256;
+		float x = catmull(a.x, b.x, c.x, d.x, t);
+		float y = catmull(a.y, b.y, c.y, d.y, t);
+		arr.append(sf::Vertex( sf::Vector2f(x,y), col));
+	}
+	window.draw(arr);
+}
+
+void drawGround(sf::RenderWindow& window) {
+	sf::VertexArray arr;
+	arr.setPrimitiveType(sf::LineStrip);
+	sf::Color col = sf::Color::Yellow;
+
+	float baseline = 600+60;
+
+	sf::Vector2f a(0, baseline);
+	sf::Vector2f b(window.getSize().x, baseline);
+
+	arr.append(sf::Vertex(a, col));
+	arr.append(sf::Vertex(b, col));
+
+	window.draw(arr);
+}
+
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(1280, 720), "SFML works!");
@@ -22,6 +86,11 @@ int main()
 	sf::RectangleShape shape(sf::Vector2f(25,60));
 	shape.setFillColor(sf::Color::Green);
 	shape.setPosition(800, 600);
+
+	sf::RectangleShape gun(sf::Vector2f(8, 32));
+	gun.setFillColor(sf::Color(	0xFF,0x00,0x00));
+	gun.setOrigin(4,0);
+	gun.setPosition(800, 600);
 
 	sf::Font fArial;
 	if (!fArial.loadFromFile("res/arial.ttf"))	
@@ -50,50 +119,64 @@ int main()
 				window.close();
 		}
 		auto pos = shape.getPosition();
-		float deltaX = dt * 40;
-		float deltaY = dt * 40;
+		float deltaX = dt * 160;
+		float deltaY = dt * 160;
 		bool keyHit = false;
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)|| sf::Keyboard::isKeyPressed(sf::Keyboard::Q)) {
 			pos.x -= deltaX;
 			keyHit = true;
 		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) || sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
 			pos.x += deltaX;
 			keyHit = true;
 		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+		/*
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) || sf::Keyboard::isKeyPressed(sf::Keyboard::Z)) {
 			pos.y -= deltaY;
 			keyHit = true;
 		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) {
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) || sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
 			pos.y += deltaY;
 			keyHit = true;
 		}
+		*/
 		if(keyHit)
 			shape.setPosition(pos);
+
 
 		bool mouseLeftIsPressed = sf::Mouse::isButtonPressed(sf::Mouse::Left);
 		bool mouseIsReleased = (mouseLeftIsPressed && !mouseLeftWasPressed);
 
 		sf::Vector2f mousePos = sf::Vector2f(sf::Mouse::getPosition(window));
-		if (mouseIsReleased) 
-			c.addPoint(mousePos);
+		if (mouseIsReleased) c.addPoint(mousePos);
+		if (sf::Mouse::isButtonPressed(sf::Mouse::Right)) c.clear();
 
 		if (mouseLeftIsPressed) 
 			mouseLeftWasPressed = true;
 		else
 			mouseLeftWasPressed = false;
-		if (sf::Mouse::isButtonPressed(sf::Mouse::Right)) {
-			c.clear();
-		}
+
+		//calculate angle from char to mouse
+		sf::Vector2f characterToMouse(
+			mousePos.y - shape.getPosition().y,
+			mousePos.x - shape.getPosition().x);
+
+		float radToDeg = 57.2958;
+		float angleC2M = atan2(characterToMouse.y, characterToMouse.x);
+		gun.setRotation(-angleC2M * radToDeg);
+		gun.setPosition(shape.getPosition() + sf::Vector2f(8, 16));
+		///
 
 		ptr.setPosition(mousePos);
-
 		tDt.setString( to_string(dt)+" FPS:"+ to_string((int)(1.0f / dt)));
 		window.clear();
 		
+		drawMountain(window);
+		drawGround(window);
+
 		//game elems
 		window.draw(shape);
+		window.draw(gun);
 		
 		c.draw(window);
 		window.draw(ptr);
